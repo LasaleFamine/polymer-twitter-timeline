de7d518a71f5fdc450593c14156c741f34e44e32
LasaleFamine <info@godev.space>
2017-09-03 16:47:46 +0200

    Added new test and fixed others, and new demo

---
+++ test/webpack.config.js
@@ -0,0 +1,10 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.js',
+  devtool: 'source-map',
+  output: {
+    path: path.resolve(__dirname),
+    filename: 'element-bundle.js'
+  }
+};
